f696055a9205a8f14ac79d6a7d016cfad11d3cdd
Jonas Broms <jonas@odduse.com>
2026-05-11 23:55:17 +0200

    feat(bot): wire up entrypoint with loaders, dispatcher, scheduler

---
+++ apps/bot/src/index.ts
@@ -0,0 +1,27 @@
+import { env } from "@repo/config";
+import { createLogger } from "@repo/logger";
+import { createClient } from "./client.js";
+import { handleError } from "./lib/errorHandler.js";
+import { loadCommands } from "./loaders/commands.js";
+import { loadComponents } from "./loaders/components.js";
+import { loadEvents } from "./loaders/events.js";
+import { loadJobs } from "./loaders/jobs.js";
+import { registries } from "./events/interactionCreate.js";
+import { startScheduler } from "./lib/scheduler.js";
+
+const log = createLogger("bot:main");
+
+async function main() {
+  const client = createClient();
+  registries.commands = await loadCommands();
+  registries.components = await loadComponents();
+  await loadEvents(client);
+  const jobs = await loadJobs();
+  client.once("ready", () => { startScheduler(client, jobs); });
+  process.on("unhandledRejection", (err) => void handleError(err, { client, where: "unhandledRejection" }));
+  process.on("uncaughtException", (err) => void handleError(err, { client, where: "uncaughtException" }));
+  await client.login(env.DISCORD_TOKEN);
+  log.info("logged in");
+}
+
+void main();
